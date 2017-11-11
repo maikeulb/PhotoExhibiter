66b859faeb02c50d22f83743cebe856a304bd874
Michael Barnes <michael.sergio.barnes@gmail.com>
2017-11-11 00:26:38 -0500

    Move Account and Manage ViewModels to ViewModels folder.

--- Web/ViewModels/AccountViewModels/ForgotPasswordViewModel.cs
+++ Web/ViewModels/AccountViewModels/ForgotPasswordViewModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Web.Models.AccountViewModels
+namespace Web.ViewModels.AccountViewModels
 {
     public class ForgotPasswordViewModel
     {
--- Web/ViewModels/ManageViewModels/ExternalLoginsViewModel.cs
+++ Web/ViewModels/ManageViewModels/ExternalLoginsViewModel.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 
-namespace Web.Models.ManageViewModels
+namespace Web.ViewModels.ManageViewModels
 {
     public class ExternalLoginsViewModel
     {
