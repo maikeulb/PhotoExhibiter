df7b20b340069d4485360615f4e0ad5757e4a25e
Michael Barnes <michael.sergio.barnes@gmail.com>
2017-11-10 22:53:08 -0500

    impliment home controller and registration viewmodel

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -4,15 +4,29 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web.Data;
 using Web.Models;
 
 namespace Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var upcomingGigs = _context.Gigs
+                .Include(g => g.Photographer)
+                .Include(g => g.Genre)
+                .Where(g => g.DateTime > DateTime.Now);
+
+            return View(upcomingGigs);
         }
 
         public IActionResult About()
