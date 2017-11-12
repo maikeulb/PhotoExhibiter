ee4a2d1b5e1aee771ccb25aa893ddafc3b302a71
Michael Barnes <michael.sergio.barnes@gmail.com>
2017-11-12 13:29:56 -0500

    Impliment FolloweesController.

---
+++ Web/Controllers/FolloweesController.cs
@@ -0,0 +1,37 @@
+using Web.Models;
+using System.Linq;
+using Web.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+
+namespace Web.Controllers
+{
+    public class FolloweesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+
+        public FolloweesController(ApplicationDbContext context,
+                UserManager<ApplicationUser> userManager,
+                SignInManager<ApplicationUser> signInManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            var userId = _userManager.GetUserId(User);
+            var photographers = _context.Followings
+                .Where(f => f.FollowerId == userId)
+                .Select(f => f.Followee)
+                .ToList();
+
+            return View(photographers);
+        }
+
+    }
+}
