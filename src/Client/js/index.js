9340fdd4a9c636d7f2d8dd2171c9977e2fa530cd
Michael Barnes <michael.sergio.barnes@gmail.com>
2017-11-28 22:16:31 -0500

    Add Typescript.

--- src/Client/js/index.js
+++ src/Client/js/index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 import $ from 'jquery';
 window.jQuery = $;
@@ -12,11 +12,12 @@ window._ = _;
 
 import bootbox from 'bootbox';
 window.bootbox = bootbox;
-window.bootbox = bootbox;
 
 import moment from 'moment';
-window.moment = moment;
 window.moment = moment;
 
 require('popper.js');
 require('bootstrap');
+
+require('bootstrap/dist/css/bootstrap.min.css');
+require('../css/site.css');
--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,33 +1,49 @@
-const path = require('path');  
-var webpack = require("webpack");
+const path = require('path');
+var webpack = require('webpack');
 
-module.exports = {  
-    context: __dirname + '/Scripts',
-    entry: {
-        index: './index.js',
-    },
-    resolve: {
-        extensions: [".js"]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-            },
-        ]
-    },
+module.exports = {
+  context: __dirname + '/Client',
+  devtool: 'eval-source-map',
+  entry: {
+    index: './js/index.js'
+  },
+  resolve: {
+    extensions: ['.js', '.css', '.ts']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
+      }
+    ]
+  },
   plugins: [
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    'window.jQuery': 'jquery',
-    moment: "moment",
-    Popper: ['popper.js', 'default']
-  })
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default']
+    })
   ],
-    output: {
-        filename: '[name].js',
-        path: __dirname + '/wwwroot/js/',
-    }
+
+  output: {
+    filename: '[name].js',
+    path: __dirname + '/wwwroot/js/'
+  }
 };
